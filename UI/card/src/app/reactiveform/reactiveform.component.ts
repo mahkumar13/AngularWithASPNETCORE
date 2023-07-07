45dbb54b39ffbf1fbd2aec15ce1731e83b85789d
mahkumar13 <getmahi25@gmail.com>
2023-07-07 23:14:13 +0530

    Added error handling

--- UI/card/src/app/reactiveform/reactiveform.component.ts
+++ UI/card/src/app/reactiveform/reactiveform.component.ts
@@ -1,8 +1,10 @@
 import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CardsService } from '../Service/cards.service';
 import { Card } from '../Models/card.model';
+import { Dbopration } from '../db.opration';
+
 
 @Component({
   selector: 'app-reactiveform',
@@ -14,6 +16,8 @@ export class ReactiveformComponent implements OnInit {
   RegisterForm:FormGroup;
   cards:Card[]=[];
   buttonTex="Save";
+  Submitted:boolean=false;
+  dbops:Dbopration;
   constructor( private _fb:FormBuilder,private _cardService:CardsService,private _toaster:ToastrService) { }
 
   ngOnInit(): void {
@@ -23,16 +27,19 @@ export class ReactiveformComponent implements OnInit {
   setRegisterForm(){
     this.RegisterForm= this._fb.group({
       id:[''],
-      cardHolderName:[""],
-      cardNumber:[""],
-      cvc:[""],
-      expiryMonth:[""],
-      expirtYear:[""]
+      cardHolderName:["",Validators.compose([Validators.required])],
+      cardNumber:["",Validators.compose([Validators.required])],
+      cvc:["",Validators.compose([Validators.required,Validators.maxLength(3)])],
+      expiryMonth:["",Validators.compose([Validators.required,Validators.maxLength(2)])],
+      expirtYear:["",Validators.compose([Validators.required,Validators.maxLength(4)])]
     })
   }
   submit(){
-    if(this.RegisterForm.controls['id'].value===""){
-
+    this.Submitted=true;
+    if(this.RegisterForm.invalid){
+      return;
+    }
+    else if(this.RegisterForm.controls['id'].value===""){
       this._cardService.addCard(this.RegisterForm.value).subscribe(res=>{
         this._toaster.success("Your card details has been added successfully")
        this.getAllCards();
@@ -45,8 +52,10 @@ export class ReactiveformComponent implements OnInit {
         expirtYear: ''
        })
       })
+      this.Submitted=false;
     }
     else{
+      debugger
       this.updateCard(this.RegisterForm.value);
      }
   }
@@ -67,6 +76,7 @@ export class ReactiveformComponent implements OnInit {
       })
       this.buttonTex="Save"
     })
+    this.Submitted=false;
   }
   getAllCards(){
     this._cardService.getAllCards().subscribe(res=>{
@@ -85,6 +95,9 @@ export class ReactiveformComponent implements OnInit {
       this._toaster.success("Your Card detalis has been deleted successfully")
       this.getAllCards();
     })
+ }
+ get ctrl(){
+  return this.RegisterForm.controls;
  }
   }
 
